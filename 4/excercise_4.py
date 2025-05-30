b2e908f56889f0842cac6be8efd79149124aea9c
egor.l@all3dmodels.com <reva140ms@gmail.com>
2025-05-30 21:50:47 +0400

    done without reversing first tower

--- 4/excercise_4.py
+++ 4/excercise_4.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter import Frame, Canvas, Text, Button, BOTH, Label
 import random
-from collections import deque
+from copy import deepcopy
 
 RINGS = 30
 STEP = 130
-HEIGHT = 10
+DISK_HEIGHT = 10
 x0 = 100
-y0 = RINGS * HEIGHT + 50
+y0 = RINGS * DISK_HEIGHT + 50
 SHOW_LABEL= "#ff0000"
 
 allowed_transitions = {
@@ -26,12 +26,12 @@ class App(Frame):
     super(App, self).__init__()
     self.master.title('Ханойские башни')
     self.student_id = '70194603'
-    self.disks = []
-    self.total_disks = 0
+    self.disks = [] # [(31, '#348755'), 0, 2] disk_data, row, tower
     self.total_iterations = 0
     self.current_iteration = 0
     self.iterations_log = []
     self.initial_disks = []
+    self.floating_disk = () # [(31, '#348755'), 0, 2] disk_data, from_, old_row, to_
 
     self.canvas = Canvas(self.master)
     self.input_id = Text(self.master, height=12, width=40)
@@ -53,7 +53,7 @@ class App(Frame):
       label.place(x=x0 + i * STEP - 10, y=y0 + 15, width=20, height=20)
 
     x = (STEP * 8 + 120)/2
-    y = HEIGHT + 440
+    y = DISK_HEIGHT + 440
     H = 50
 
     self.button_start.place(x=x-H, y=y-20, width=120, height=40)
@@ -79,20 +79,6 @@ class App(Frame):
     self.input_pos3.place(x=x+4*H, y=y-40, width=50, height=40)
     self.input_pos4.place(x=x+5*H, y=y-40, width=50, height=40)
 
-    self.list_labels = list()
-    max_item = 9
-    for row in range(8*max_item):
-      line = list()
-      for tower in range(8):
-        label = Label(self.master, text="0")
-        x = x0 - STEP/2 + tower * STEP
-        y = y0 - (row + 1) * HEIGHT
-        label.config(font=("Courier", 7))
-        label.place(x=x, y=y, width=10, height=7)
-        label["fg"] = self.master["bg"]
-        line.append(label)
-      self.list_labels.append(line)
-
     self.get_initial_state()
     self.total_iterations = self.calculate_total_iterations()
     print(f"Общее количество итераций: {self.total_iterations}")
@@ -116,10 +102,9 @@ class App(Frame):
         self.towers[row][tower] = (m * 10 + n, color)
         self.disks.append([self.towers[row][tower], row, tower])
 
-    self.initial_towers = self.towers.copy()
+    self.floating_disk = ()
+    self.initial_towers = deepcopy(self.towers)
     self.disks = sorted(self.disks, key=lambda x: x[0])
-    self.total_disks = len(self.disks)
-      
 
   def draw_towers(self):
     self.canvas.delete("all")
@@ -127,10 +112,7 @@ class App(Frame):
     for display_tower in range(8):
       real_tower = 7 - display_tower
       for row in range(RINGS):
-        if self.towers[row][real_tower][0] == 0:
-          self.list_labels[row][display_tower].config(text="0")
-          self.list_labels[row][display_tower].config(fg=self.master["bg"])
-          self.list_labels[row][display_tower].config(state='normal')
+        if self.towers[row][real_tower][0] == 0 or (self.floating_disk and self.towers[row][real_tower] == self.floating_disk[0]):
           continue
         line = self.towers[row][real_tower]
         if isinstance(line[0], tuple):
@@ -138,32 +120,35 @@ class App(Frame):
         w = line[0]
         color = line[1]
         x = x0 + display_tower * STEP - w / 2
-        y = y0 - HEIGHT * (row + 1)
-        self.canvas.create_rectangle(x, y, x + w, y + HEIGHT, outline=color, fill=color)
-        self.list_labels[row][display_tower].config(text=str(w))
-        self.list_labels[row][display_tower].config(fg=SHOW_LABEL)
-        self.list_labels[row][display_tower].config(state='normal')
+        y = y0 - DISK_HEIGHT * (row + 1)
+        self.canvas.create_rectangle(x, y, x + w, y + DISK_HEIGHT, outline=color, fill=color)
+        self.canvas.create_text(x + w / 2, y + DISK_HEIGHT / 2, text=str(w), fill=SHOW_LABEL, font=("Courier", 10, "bold"))
+
     self.canvas.pack(fill=BOTH, expand=1)
+    if self.floating_disk:
+      self.draw_floating_disk(self.floating_disk)
 
   def draw_base(self):
       color = "#ffffff"
-      H = RINGS * HEIGHT
+      H = RINGS * DISK_HEIGHT
       x1 = x0+STEP*8
       y1 = y0+10
 
-      self.canvas.create_rectangle(x0-70, y0, x1-70, y1, outline="#fb0", fill=color)
+      self.canvas.create_rectangle(x0-70, y0, x1-70, y1, outline="#0bf", fill=color)
       self.canvas.pack(fill=BOTH, expand=1)
       for i in range(8):
         x1 = int(x0 - 3 + i * STEP)
         y1 = int(y0 - H)
         x2 = x1 + 6
         y2 = y0
-        self.canvas.create_rectangle(x1, y1, x2, y2, outline="#fb0", fill=color)
+        self.canvas.create_rectangle(x1, y1, x2, y2, outline="#0bf", fill=color)
         self.canvas.pack(fill=BOTH, expand=1)
 
   def draw_iteration(self, percent):
+    self.floating_disk = ()
     target_iteration = percent / 100 * self.total_iterations
-    self.towers = self.initial_towers.copy()
+    self.towers = deepcopy(self.initial_towers)
+    is_float = False
 
     if '.' in str(target_iteration):
       before_comma, after_comma = str(target_iteration).split('.')
@@ -173,46 +158,47 @@ class App(Frame):
       else:
         target_iteration_int = int(before_comma)
         target_iteration = float(before_comma + '.' + after_comma[:3])
+        is_float = True
 
     self.current_iteration_label.config(text=f"Текущая итерация {target_iteration}")
 
-    
-    self.process_iterations(target_iteration_int)
+    if is_float:
+      self.process_iterations(target_iteration_int - 1)
+    else:
+      self.process_iterations(target_iteration_int)
+
+    if is_float:
+      disk, from_, old_row, to_, new_row = self.iterations_log[target_iteration_int] # (diameter, color), tower_index, row_index, tower_index, row_index
+      self.floating_disk = (disk, from_, old_row, to_)
+
     self.draw_towers()
 
   def process_iterations(self, target_iteration):
-    self.towers = [[(0, "#000000") for _ in range(8)] for _ in range(72)]
+    if target_iteration < 1: return
+    self.towers = deepcopy(self.initial_towers)
 
     for iteration in self.iterations_log[:target_iteration]:
-      disk, from_, to_ = iteration
-
-      if self.can_place_on_top(disk, to_):
-        for row in range(72):
-          if self.towers[row][from_] == disk:
-            self.towers[row][from_] = (0, "#000000")
-            break
-        for row in range(72):
-          if self.towers[row][to_] == (0, "#000000"):
-            self.towers[row][to_] = disk
-            break
-
+      disk, from_, old_row, to_, new_row = iteration # (diameter, color), tower_index, row_index, tower_index, row_index
+      self.towers[old_row][from_] = (0, "#000000")
+      self.towers[new_row][to_] = disk
+      
 
   def draw_end(self):
-    self.towers = [[(0, "#000000") for _ in range(8)] for _ in range(72)]
-    for i, disk in enumerate(self.disks):
-        self.towers[i][0] = disk
-    self.draw_towers()
+    self.draw_iteration(100)
 
-
-
-  def draw_floating_disk(self, disk, from_, to_):
+  def draw_floating_disk(self, data):
+    disk, from_, old_row, to_ = data
+    self.towers[old_row][from_] = (0, '#000000')
     w = disk[0]
     color = disk[1]
     x_from = x0 + (7 - from_) * STEP
     x_to = x0 + (7 - to_) * STEP
     x = (x_from + x_to) / 2 - w / 2
-    y = y0 - RINGS * HEIGHT - 30
-    self.canvas.create_rectangle(x, y, x + w, y + HEIGHT, outline=color, fill=color)
+    y = y0 - RINGS * DISK_HEIGHT - 30
+    rectangle_id = self.canvas.create_rectangle(x, y, x + w, y + DISK_HEIGHT, outline=color, fill=color)
+    text_id = self.canvas.create_text(x + w / 2, y + DISK_HEIGHT / 2, text=str(w), fill=SHOW_LABEL, font=("Courier", 10, "bold"))
+    self.canvas.tag_raise(rectangle_id)
+    self.canvas.tag_raise(text_id)
 
   def find_tower(self, disk):
     for tower in range(8):
@@ -221,71 +207,59 @@ class App(Frame):
           return tower
     return None
 
-  def can_place_on_top(self, disk, tower_index):
+  def can_place_on_top(self, diameter, target_tower):
     for row in range(72):
-      if self.towers[row][tower_index][0] == 0:
+      if self.towers[row][target_tower][0] == 0:
         if row == 0:
           return True
-        below_disk = self.towers[row - 1][tower_index]
-        return below_disk[0] > disk[0]
+        below_disk = self.towers[row - 1][target_tower]
+        return below_disk[0] < diameter
+    return False
+  
+  def move_disk_to_first_tower(self, disk):
+    disk_data = disk[0]
+    row = disk[1]
+    tower = disk[2]
+
+    n = 1
+    if tower == 7:
+      n = 2
+
+    # выйти с рекурсии если диск на первой башне
+    if tower == 0: return
+
+    if self.can_place_on_top(disk_data[0], tower - n):
+      available_place = self.find_top_available_place(tower - n)
+      if available_place >= 0:
+        self.towers[row][tower] = (0, '#000000')
+        
+        self.towers[available_place][tower - n] = disk_data
+        self.iterations_log.append((disk_data, tower, row, tower-n, available_place))
+        self.total += 1
+
+        self.move_disk_to_first_tower([disk_data, available_place, tower - n])
+        
+  def change_disk_data(self, new_disk_data):
+    disk_data = new_disk_data[0]
+
+    for disk in self.disks:
+      if disk[0][0] == disk_data[0]:
+        disk = new_disk_data
+
+  def find_top_available_place(self, tower_index):
+    for row in range(72):
+      if self.towers[row][tower_index][0] == 0:
+        return row
     return False
 
-  def find_path_route(self, from_index, to_index, disk):
-    from_peg = from_index + 1
-    to_peg = to_index + 1
-
-    max_steps = 500
-    steps = 0
-    visited = set()
-    parent = {from_peg: None}
-    queue = deque([from_peg])
-
-    path_found = False
-
-    while queue and steps < max_steps:
-      steps += 1
-      current = queue.popleft()
-      if current == to_peg:
-        path_found = True
-        break
-      for neighbor in allowed_transitions.get(current, []):
-        if neighbor not in visited:
-          if self.can_place_on_top(disk[0], neighbor - 1):
-            visited.add(neighbor)
-            parent[neighbor] = current
-            queue.append(neighbor)
-
-    if not path_found:
-      return []
-
-    path = []
-    current = to_peg
-    while current is not None:
-      path.append(current - 1)
-      current = parent.get(current)
-
-    return path[::-1]
-
   def calculate_total_iterations(self):
-    self.moves_log = []
-    total = 0
+    self.iterations_log = []
+    self.total = 0
     for disk in self.disks:
-      print(f"Диск {disk} с башни {disk[2] + 1}")
-      if disk[0] == 0:
-          continue
-      from_tower = disk[2]
-      to_tower = 0
-      if from_tower == to_tower:
-          continue
-      path = self.find_path_route(from_tower, to_tower, disk)
-      if not path:
-          print(f"⚠️ Нет допустимого пути для диска {disk} с башни {from_tower + 1} на башню {to_tower + 1}")
-          continue
-      for i in range(len(path) - 1):
-          self.moves_log.append((disk, path[i], path[i + 1]))
-      total += len(path) - 1
-    print(f"✅ Всего найдено шагов: {total}")
-    return total
+      if disk[0][0] < 20: continue
+      self.move_disk_to_first_tower(disk)
+
+    return len(self.iterations_log)
 
   def draw_pos1(self):
     p = int(self.input_pos1.get("1.0", "end").replace("\n", ""))
@@ -307,6 +281,6 @@ if __name__ == '__main__':
   root = tk.Tk()
   app = App()
   w = STEP * 10 + 30
-  h = RINGS * HEIGHT + 20 + 200
+  h = RINGS * DISK_HEIGHT + 20 + 200
   root.geometry(f"{w}x{h}")
   root.mainloop()
